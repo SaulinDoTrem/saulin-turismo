e6d4ee9084b04409bfd049c3a8ed059b5bd82bf7
Saulo Klein Nery <saulokn@outlook.com>
2023-03-08 17:33:13 -0300

    feat: Começando a criar a home

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,14 @@ const source = require("vinyl-source-stream");
 const uglify = require("gulp-uglify");
 const buffer = require("vinyl-buffer");
 const connect = require("gulp-connect");
+const imagemin = require("gulp-imagemin");
 
 const paths = {
     html: {
         all: "src/templates/**/*.html",
+    },
+    images: {
+        all: "src/assets/**/*",
     },
     styles: {
         all: "src/styles/**/*.scss",
@@ -18,25 +22,35 @@ const paths = {
         all: "src/scripts/**/*.js",
         main: "src/scripts/app.js",
     },
-    output: "docs",
+    output: {
+        files: "docs",
+        images: "docs/assets",
+    },
 };
 
 function server() {
     connect.server({
-        root: paths.output,
+        root: paths.output.files,
         livereload: true,
-        port: 3001,
+        port: 3000,
     });
 }
 
 function html() {
-    return src(paths.html.all).pipe(dest(paths.output)).pipe(connect.reload());
+    return src(paths.html.all).pipe(dest(paths.output.files)).pipe(connect.reload());
+}
+
+function images() {
+    return src(paths.images.all)
+        .pipe(imagemin())
+        .pipe(dest(paths.output.images))
+        .pipe(connect.reload());
 }
 
 function styles() {
     return src(paths.styles.main)
         .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-        .pipe(dest(paths.output))
+        .pipe(dest(paths.output.files))
         .pipe(connect.reload());
 }
 
@@ -47,7 +61,7 @@ function scripts() {
         .pipe(source("bundle.js"))
         .pipe(buffer())
         .pipe(uglify())
-        .pipe(dest(paths.output))
+        .pipe(dest(paths.output.files))
         .pipe(connect.reload());
 }
 
@@ -55,6 +69,7 @@ function sentinel() {
     watch(paths.html.all, { ignoreInitial: false }, html);
     watch(paths.styles.all, { ignoreInitial: false }, styles);
     watch(paths.scripts.all, { ignoreInitial: false }, scripts);
+    watch(paths.images.all, { ignoreInitial: false }, images);
 }
 
 exports.default = parallel(server, sentinel);
